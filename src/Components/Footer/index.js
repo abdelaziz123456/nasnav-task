8675f30c8ad1b157cac6ff371bd81fa436a6990e
abdelaziz mohamed <62770432+abdelaziz123456@users.noreply.github.com>
2022-01-18 14:57:03 +0200

    add header and footer components

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,106 @@
+import { Telegram } from "@material-ui/icons";
+import React from "react";
+import "./index.scss";
+export default function Footer() {
+  return (
+    <div className="footer">
+      {/* top footer start */}
+      <div className="row top-footer">
+        {/* left section start */}
+
+        <div className="left col-12 col-lg-6 my-3">
+          <img
+            src="img/footer/yeshtery.png"
+            alt=""
+            width="220"
+            height="58"
+            className="my-3"
+          />
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat.
+          </p>
+          <p>
+            {" "}
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+            suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
+            autem vel eum iriure dolor in hendrerit in vulputate velit esse
+            molestie consequat, vel illum dolore eu feugiat nulla. Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed dia m nonummy nibh
+            euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.{" "}
+          </p>
+          <p>
+            esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+            at vero eros et accumsan et iusto odio dignissim qui blandit
+          </p>
+        </div>
+
+        {/* left section end */}
+
+        {/* right section start */}
+
+        <div className="right col-12 col-lg-6 ps-5 my-3">
+          <h6 className="my-2">Subscribe to our newsletter</h6>
+
+          <div className="d-flex align-items-center subscribe">
+          <input
+              type="email"
+              class="form-control "
+              
+              placeholder="Enter Your Email"
+            />
+            <button className="btn d-flex  ">Subscribe <Telegram fontSize="small"/></button>
+            
+          </div>
+
+
+          <div className="row  mt-4">
+            <div className="col-6">
+              <p>About Us</p>
+              <p>Contact Us</p>
+              <p>Track Order</p>
+              <p>Terms & Condition</p>
+              <p>Privacy Policy</p>
+              <p>Sell With Us</p>
+              <p>Shipping And Returns</p>
+            </div>
+            <div className="col-6 links ps-5">
+              <p><img src="/img/footer/facebook.png" alt="" /> /YESHTERY</p>
+              <p><img src="/img/footer/linkedin.png" alt="" />/YESHTERY</p>
+              <p><img src="/img/footer/instagram.png" alt="" />/YESHTERY</p>
+              <p><img src="/img/footer/twitter.png" alt="" />/YESHTERY</p>
+            </div>
+          </div>
+
+
+        </div>
+
+        {/* right section end */}
+      </div>
+
+      {/* top footer end */}
+
+      {/* bottom footer start */}
+
+      <div className="bottom-footer d-flex justify-content-between align-items-center my-3">
+
+        <small className="fw-light"> &#169; 2021 yesterday. all rights reserved	</small>
+
+        <div className="d-flex">
+          <img src="img/footer/cash.png" alt="" height='30' className="mx-1" />
+          <img src="img/footer/visa.png" alt="" height='30' className="mx-1" />
+          <img src="img/footer/master.png" alt="" height='30' className="mx-1" />
+        </div>
+
+
+        <p className="fw-light mb-0">powerd by <img src="img/footer/nasnav.png" alt="" /></p>
+
+
+      </div>
+
+      {/* bottom footer end */}
+    </div>
+  );
+}
---
+++ src/Components/Header/index.js
@@ -0,0 +1,136 @@
+import {
+  AddIcCallOutlined,
+  ShoppingCartOutlined,
+  RoomOutlined,
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+  SearchOutlined,
+  ShoppingBasketOutlined,
+  FavoriteBorderOutlined,
+  PersonOutlineOutlined,
+} from "@material-ui/icons";
+import ListIcon from "@material-ui/icons/List";
+import React from "react";
+import "./index.scss";
+export default function Header() {
+  return (
+    <div className="header">
+      {/* top header start */}
+
+      <div className="top-header  d-flex justify-content-between  ">
+        <div className="left">
+          <ListIcon />
+          <img src="/img/yeshtery.svg" alt="" />
+        </div>
+
+        <div className="center">
+          <p className="m-0">
+            {" "}
+            <ArrowBackIosOutlined fontSize="small" /> Valentine's Day Offers Buy
+            Two Get One Free <ins>Shop Now</ins>{" "}
+            <ArrowForwardIosOutlined fontSize="small" />{" "}
+          </p>
+        </div>
+
+        <div className="right d-flex  ">
+          <p>
+            <AddIcCallOutlined fontSize="small" />
+            <span>Contact Us</span>
+          </p>
+
+          <p>
+            <ShoppingCartOutlined fontSize="small" />
+            <span>Track Order</span>
+          </p>
+          <p>
+            <RoomOutlined fontSize="small" />
+            <span>Find A Store</span>
+          </p>
+        </div>
+      </div>
+
+      {/* top header end  */}
+
+
+
+
+
+      {/* center header start */}
+
+      <div className="mid-header  d-flex justify-content-between align-items-center py-2">
+
+
+        <div className="left d-flex align-items-center">
+            <label htmlFor="" className="form-label">
+                <SearchOutlined fontSize="large"/>
+            </label>
+        <input type="search" id="form1" class="form-control" placeholder="Search" />
+        </div>
+        <div className="center">
+          <img src="/img/addidas.png" height="57" alt="" />
+        </div>
+        <div className="right d-flex  ">
+          <p>
+            <ShoppingBasketOutlined  />
+            <span>Cart</span>
+          </p>
+
+          <p>
+            <FavoriteBorderOutlined/>
+            <span>Wishlist</span>
+          </p>
+          <p>
+            <PersonOutlineOutlined />
+            <span>Login</span>
+          </p>
+        </div>
+      </div>
+
+      {/* center header end */}
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* bottom header start */}
+
+      <div className="bottom-header d-flex justify-content-center ">
+          <div className="d-flex justify-content-between w-100 ">
+            <p>Men</p>
+            <p>Women</p>
+            <p>Unisex</p>
+            <p>Kids</p>
+            <p>Best Sellers</p>
+            <p>New Arrivals</p>
+            <p>Offers</p>
+
+          </div>
+        
+     </div>
+
+
+     {/* bottom header end */}
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
